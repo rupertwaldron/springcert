a609db479c85e1346d3f9cc04a023b1c5a15ce2e
rupertwaldron <rupert.waldron@yahoo.co.uk>
2020-05-30 07:42:02 +0100

    added counter metrics for users and credentials

--- src/main/java/com/ruppyrup/springcert/service/impl/CredentialServiceImpl.java
+++ src/main/java/com/ruppyrup/springcert/service/impl/CredentialServiceImpl.java
@@ -7,19 +7,33 @@ import com.ruppyrup.springcert.jwt.JwtContextManager;
 import com.ruppyrup.springcert.model.Credential;
 import com.ruppyrup.springcert.model.CredentialDTO;
 import com.ruppyrup.springcert.service.CredentialService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 public class CredentialServiceImpl implements CredentialService {
+
+    private Counter credentialCount;
 
     @Autowired
     CredentialDao credentialDao;
 
     @Autowired
     JwtContextManager jwtContextManager;
+
+    public CredentialServiceImpl(MeterRegistry meterRegistry) {
+        credentialCount = meterRegistry.counter("storage.credential.count", "type", "credential");
+    }
+
+    @PostConstruct
+    public void countCredentials() {
+        credentialDao.findAll().forEach(cred -> credentialCount.increment());
+    }
 
     @Override
     public List<Credential> getAllCredentials() {
@@ -36,6 +50,7 @@ public class CredentialServiceImpl implements CredentialService {
     public Credential createCredential(CredentialDTO credentialDto) {
         Credential credential = new Credential(credentialDto);
         credential.setUser(jwtContextManager.getAuthorizedUser());
+        credentialCount.increment();
         return credentialDao.save(credential);
     }
 
@@ -54,6 +69,7 @@ public class CredentialServiceImpl implements CredentialService {
         Credential credentialToDelete = getAuthorizedCredential(uuid);
         if (credentialToDelete == null) return null;
         credentialDao.delete(credentialToDelete);
+        credentialCount.increment(-1);
         return credentialToDelete;
     }
 
--- src/main/java/com/ruppyrup/springcert/service/impl/JwtUserDetailsService.java
+++ src/main/java/com/ruppyrup/springcert/service/impl/JwtUserDetailsService.java
@@ -4,6 +4,8 @@ import com.ruppyrup.springcert.dao.UserDao;
 import com.ruppyrup.springcert.exceptions.ExistingUserException;
 import com.ruppyrup.springcert.model.DAOUser;
 import com.ruppyrup.springcert.model.UserDTO;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,16 +14,28 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+
+    private Counter userCount;
 
     @Autowired
     private UserDao userDao;
 
     @Autowired
     private PasswordEncoder bcryptEncoder;
+
+    public JwtUserDetailsService(MeterRegistry meterRegistry) {
+        userCount = meterRegistry.counter("storage.user.count", "type", "user");
+    }
+
+    @PostConstruct
+    public void countUsers() {
+        userDao.findAll().forEach(cred -> userCount.increment());
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -39,12 +53,14 @@ public class JwtUserDetailsService implements UserDetailsService {
         DAOUser newUser = new DAOUser();
         newUser.setUsername(user.getUsername());
         newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        userCount.increment();
         return userDao.save(newUser);
     }
 
     public void deleteUser(UserDTO userdto) {
         DAOUser existingUser = userDao.findByUsername(userdto.getUsername());
         userDao.delete(existingUser);
+        userCount.increment(-1);
     }
 
     public DAOUser getUser(String userName) {
