6771dac35fbe525f7148afaf857d96fc0b922cf3
rupertwaldron <rupert.waldron@yahoo.co.uk>
2020-05-30 06:51:05 +0100

    added added a register file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import AddCredential from "./Components/AddCredential";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Table from "./Components/Table";
+import Register from "./Components/Register";
 
 class App extends Component {
     render() {
         return (
             <Router>
+                <Route exact path="/register" component={Register} />
                 <Route exact path="/" component={AddCredential} />
                 <Route exact path="/view" component={Table} />
             </Router>
