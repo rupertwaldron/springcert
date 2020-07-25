1be6e5245daf6b85baec92f2ec8cc2fe8cf786c1
rupertwaldron <rupert.waldron@yahoo.co.uk>
2020-07-25 16:13:21 +0100

    Added contextlogger to tests

---
+++ src/test/java/com/ruppyrup/springcert/config/ContextLoaderLoggerConfiguration.java
@@ -0,0 +1,21 @@
+package com.ruppyrup.springcert.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class ContextLoaderLoggerConfiguration implements ApplicationContextAware {
+    // checks how many context configurations are created
+
+    private static int numberOfContexts;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("CONTEXT LOADER LOGGER => Application Context No " + (++numberOfContexts) +
+                " was created at " + Objects.hashCode(applicationContext.hashCode()));
+    }
+}
